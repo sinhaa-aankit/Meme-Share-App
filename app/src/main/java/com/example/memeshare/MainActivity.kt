1dd31c2dafb48d2847d48522cf7b956d9f6f30dd
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2020-10-05 22:58:19 +0530

    Fixed some error & Implemented Volley Library to make API calls to get random memes from reddit. To show the image into ImageView used Glide Library from android documentation. Also added a ProgressBar to show when image processing & downloading is running.

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.memeshare
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,15 +12,23 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadMeme()
     }
 
     private fun loadMeme(){
+
+        progressBar.visibility = View.VISIBLE
+
         val queue = Volley.newRequestQueue(this)
         val url = "https://meme-api.herokuapp.com/gimme"
 
@@ -27,7 +36,28 @@ class MainActivity : AppCompatActivity() {
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
             { response ->
                 val url = response.getString("url")
-                Glide.with(this).load(url).into(memeImageView)
+                Glide.with(this).load(url).listener(object: RequestListener<Drawable>{
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+                }).into(memeImageView)
             }
         ) {
 
@@ -38,5 +68,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun shareMeme(view: View) {}
-    fun nextMeme(view: View) {}
+    fun nextMeme(view: View) {
+        loadMeme()
+    }
 }
